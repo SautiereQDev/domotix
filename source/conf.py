fe87379c3b8ab28c6bc93cb3e8ff9359b2513114
SautiereQDev <contact@quentinsautiere.com>
2025-07-24 19:47:15 +0200

    add Sphinx documentation configuration and makefiles

---
+++ source/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Domotix"
+copyright = "2025, Quentin Sautière"
+author = "Quentin Sautière"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions: list[str] = []
+
+templates_path = ["_templates"]
+exclude_patterns: list[str] = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
